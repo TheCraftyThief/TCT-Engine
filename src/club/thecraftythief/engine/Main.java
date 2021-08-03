91be590c29d7b81b46ff8e44518e1eae0d8f2d4d
Dominic Harris <tastykake563@gmail.com>
2021-08-02 22:37:46 -0400

    (　-_･) ︻デ═一' * (/❛o❛)/ Stupid thing is gone now

--- src/club/thecraftythief/engine/Main.java
+++ src/club/thecraftythief/engine/Main.java
@@ -2,9 +2,5 @@ package club.thecraftythief.engine;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import org.bukkit.plugin.java.JavaPluginLoader;
-
-import org.bukkit.advancement.Advancement;
-
 public class Main extends JavaPlugin {
 }
