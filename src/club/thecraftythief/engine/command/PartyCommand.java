2db14ff8bf4caab38bc7f9113dfa8d1e0838544e
Dominic Harris <tastykake563@gmail.com>
2021-08-09 19:41:17 -0400

    Work on party commands! o/

--- src/club/thecraftythief/engine/command/PartyCommand.java
+++ src/club/thecraftythief/engine/command/PartyCommand.java
@@ -1,25 +1,81 @@
 package club.thecraftythief.engine.command;
 
+import club.thecraftythief.engine.party.Party;
+import club.thecraftythief.engine.party.PartyMgr;
 import co.aikar.commands.BaseCommand;
-import co.aikar.commands.annotation.CommandAlias;
-import co.aikar.commands.annotation.Default;
-import co.aikar.commands.annotation.Subcommand;
+import co.aikar.commands.annotation.*;
+import co.aikar.commands.bukkit.contexts.OnlinePlayer;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
 @CommandAlias("party|p")
 public class PartyCommand extends BaseCommand {
-    private String helpLine(String commandName, String commandDescription){
-        return (ChatColor.AQUA+commandName+ChatColor.GRAY+ChatColor.ITALIC+commandDescription);
+    private String helpLine(String commandName, String commandDescription) {
+        return (ChatColor.LIGHT_PURPLE + "" + commandName + ChatColor.RESET + "" + ChatColor.WHITE + commandDescription);
     }
+
     @Default
+    @HelpCommand
+    @Description("Get a list of all party commands")
     public void onCommand(Player runner) {
-        runner.sendMessage(helpLine("/p accept [player]", " - Accept a party invite from a player"));
-        runner.sendMessage(helpLine("/p invite [player]", " - Invites player "));
-        //runner.sendMessage(ChatColor.AQUA+"/p invite [player]"+ChatColor.GRAY+ChatColor.ITALIC+);
+        runner.sendMessage(ChatColor.LIGHT_PURPLE + "" + ChatColor.BOLD + "Party" + ChatColor.RESET + "" + ChatColor.WHITE + " commands");
+        runner.sendMessage(helpLine("/p accept [player]", " to accept a party invite from a player"));
+        runner.sendMessage(helpLine("/p invite [player]", " to invite a player"));
+        runner.sendMessage(helpLine("/p kick [player]", " to kick a player from your party"));
+        runner.sendMessage(helpLine("/p disband [player]", " to disband your current party"));
+        runner.sendMessage(helpLine("/p leave", " to leave your current party"));
+        runner.sendMessage(helpLine("/p info", " to get your current party info"));
     }
-    @Subcommand("list")
-    public void onlist(Player runner) {
+
+    @Subcommand("accept")
+    @Description("Accept a party invite")
+    public void onAccept(Player runner) {
+        runner.sendMessage("");
+    }
+
+    @Subcommand("invite")
+    @Description("Invite a player to your party")
+    public void onInvite(Player runner, OnlinePlayer player) {
+        PartyMgr partyMgr = PartyMgr.getInstance();
+
+        Party party = partyMgr.getPlayerParty(runner);
+
+        if (party == null) {
+            if (partyMgr.getPlayerParty(runner.getPlayer()) != null) {
+                runner.sendMessage("Youre already in a party..");
+                return;
+            }
+            party = new Party(runner.getUniqueId());
+            partyMgr.addParty(party);
+        }
+
+        partyMgr.addPartyInvite(party, player.getPlayer().getUniqueId());
+
+        runner.sendMessage("Invited " + ChatColor.LIGHT_PURPLE + "" + ChatColor.BOLD + player.getPlayer().getName() + ChatColor.RESET + " to your party!");
+        player.getPlayer().sendMessage(ChatColor.LIGHT_PURPLE + "" + ChatColor.BOLD + runner.getName() + ChatColor.RESET  + " invited you to a party!");
+    }
+
+    @Subcommand("kick")
+    @Description("Kick a player from your party")
+    public void onKick(Player runner) {
+        runner.sendMessage("");
+    }
+
+    @Subcommand("disband")
+    @Description("Disband your party")
+    public void onDisband(Player runner) {
+        runner.sendMessage("");
+    }
+
+    @Subcommand("leave")
+    @Description("Leave your current party")
+    public void onLeave(Player runner) {
+        runner.sendMessage("");
+    }
+
+    @Subcommand("info")
+    @Description("Get info on your current party")
+    public void onInfo(Player runner) {
         runner.sendMessage("");
     }
 }
--- src/club/thecraftythief/engine/party/PartyMgr.java
+++ src/club/thecraftythief/engine/party/PartyMgr.java
@@ -1,26 +1,63 @@
 package club.thecraftythief.engine.party;
 
-import club.thecraftythief.engine.model.ModelMgr;
+import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
 
 public class PartyMgr {
     private static PartyMgr instance;
-    private List<Party> partyList;
+    private final List<Party> partyList;
+    private final HashMap<Party, UUID> partyInvites;
 
     public PartyMgr() {
         instance = this;
         this.partyList = new ArrayList<>();
+        this.partyInvites = new HashMap<>();
     }
 
     public static PartyMgr getInstance() {
         return instance;
     }
 
+    /**
+     * Adds a party to the list of parties
+     * @param party The party to add
+     */
     public void addParty(Party party) {
         this.partyList.add(party);
     }
 
-    public List<Party> getParties() { return this.partyList; }
+    /**
+     * Invites a player  by UUID to the party
+     * @param party The party to invite the player to
+     * @param uuid The player to invite to the party
+     */
+    public void addPartyInvite(Party party, UUID uuid) {
+        this.partyInvites.put(party, uuid);
+    }
+
+    /**
+     * Gets a list of all parties
+     * @return List<Party> List of parties
+     */
+    public List<Party> getParties() {
+        return this.partyList;
+    }
+
+    /**
+     * Gets a players current party or returns null
+     * @param player The player to get the party of
+     * @return Party The players party or null
+     */
+    public Party getPlayerParty(Player player) {
+        for (Party party : this.getParties()) {
+            if (party.getOwner() == player.getUniqueId()) {
+                return party;
+            }
+        }
+        return null;
+    }
 }
